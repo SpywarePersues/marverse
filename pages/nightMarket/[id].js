21eedc87b52986998444ba16e06f026c23e234a6
SpywarePersues <spywarepersues284@gmail.com>
2022-10-13 20:11:49 +0530

    Added the structure for seperate pages for the products of each night market item

--- pages/nightMarket/[id].js
+++ pages/nightMarket/[id].js
@@ -1,11 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
 import React, { useEffect as UseEffect, useState as UseState } from 'react'
 import ContainerBlock from '../../components/ContainerBlock'
+import { useRouter as UseRouter } from 'next/router'
+import { database } from '../../firebaseConfig'
+import { collection, getDocs, doc, getDoc } from 'firebase/firestore'
+import Link from 'next/link'
 
 function nightMarketScreen() {
+    const router = UseRouter()
+    const { id } = router.query;
+
     return (
         <ContainerBlock>
-            
+            <h1>Page no: {id}</h1>
         </ContainerBlock>
     )
 }
