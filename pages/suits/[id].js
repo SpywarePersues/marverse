f023a3977424d4c8af67e3e6ae4fc3a282d87699
SpywarePersues <spywarepersues284@gmail.com>
2022-10-13 21:08:34 +0530

    Added outer structure for all of the slug pages

---
+++ pages/suits/[id].js
@@ -0,0 +1,20 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useEffect as UseEffect, useState as UseState } from 'react'
+import ContainerBlock from '../../components/ContainerBlock'
+import { useRouter as UseRouter } from 'next/router'
+import { database } from '../../firebaseConfig'
+import { collection, getDocs, doc, getDoc } from 'firebase/firestore'
+import Link from 'next/link'
+
+function suitsScreen() {
+    const router = UseRouter()
+    const { id } = router.query;
+
+    return (
+        <ContainerBlock>
+            <h1>Page no: {id}</h1>
+        </ContainerBlock>
+    )
+}
+
+export default suitsScreen
