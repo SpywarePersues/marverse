78c61e50876351704b51bbd99a7f47a7149e32ec
SpywarePersues <spywarepersues284@gmail.com>
2022-10-13 14:55:29 +0530

    Added login and profile page

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,21 @@
 import Link from "next/link"
+import React, { useEffect as UseEffect, useState as UseState } from 'react'
 
 export default function Navbar(){
+    const [token, setToken] = UseState("")
+
+    UseEffect(() => {
+        setToken(sessionStorage.getItem('Token'))
+    }, [])
     return(
         <div className="py-5">
-            <ul className="lg:flex justify-between w-9/12 text-center text-base cursor-pointer lg:w-7/12 mx-auto py-5 text-gray-200 glassmorph rounded-md lg:px-36">
+            <ul className="lg:flex justify-between w-10/12 text-center text-base cursor-pointer lg:w-8/12 mx-auto py-5 text-gray-200 glassmorph rounded-md lg:px-36">
                 <Link href={{pathname: '/'}}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Home</li></Link>
                 <Link href={{pathname: '/shop'}}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Shop</li></Link>
                 <Link href={{pathname: '/nightMarket'}}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Night Market</li></Link>
                 <Link href={{pathname: '/infinityStones'}}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Infinity stones</li></Link>
                 <Link href={{pathname: '/ironmanSuits'}}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Iron Man Suits</li></Link>
+                {token ? (<Link href={{pathname: '/login'}}><img src={sessionStorage.getItem('PFP')} className=" w-8 h-8 rounded-full mx-auto my-2 lg:my-0 lg:mx-0"/></Link>) : (<Link href={{pathname: '/login'}}><li className="hover:text-gray-100 hover:bg-gray-500 px-4 py-[0.35rem] rounded-md transition-all duration-300 cursor-pointer">Login</li></Link>)}
             </ul>
         </div>
     )
