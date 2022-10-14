7660316215ea1dd487758db598c5e781eb0ff98c
Siddharth <sid.sharma1704@gmail.com>
2022-10-14 20:42:11 +0530

    product pages

--- pages/nightMarket/[id].js
+++ pages/nightMarket/[id].js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useEffect as UseEffect, useState as UseState } from 'react'
+import React, { useEffect, useEffect as UseEffect, useState, useState as UseState } from 'react'
 import ContainerBlock from '../../components/ContainerBlock'
 import { useRouter as UseRouter } from 'next/router'
 import { database } from '../../firebaseConfig'
@@ -10,9 +10,43 @@ function nightMarketScreen() {
     const router = UseRouter()
     const { id } = router.query;
 
+    const databaseRef = collection(database, 'Night Market')
+    const [firedata, setFiredata] = UseState([])
+    const [currentData, setCurrentData] = useState({})
+
+    UseEffect(() => {
+        getData()
+    }, [])
+
+    const htmlJSX = (
+        <div>{currentData.Name}</div>
+    )
+
+    const getData = async () => {
+        await getDocs(databaseRef)
+        .then((response) => {
+            setFiredata(response.docs.map((data) => {
+                return {...data.data(), id: data.id}
+            }))
+        })
+    }
+
+    useEffect(() =>{
+        firedata.map((data) => {
+            if(data.id == id){
+                setCurrentData(data)
+            }
+        })
+
+
+    
+    }, [firedata])
+
+    console.log(currentData)
+
     return (
         <ContainerBlock>
-            
+            {htmlJSX}
         </ContainerBlock>
     )
 }
