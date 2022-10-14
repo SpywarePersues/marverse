43ad329036af2e2d7b5db3ff68cc0c64b422ed8a
SpywarePersues <spywarepersues284@gmail.com>
2022-10-15 00:21:21 +0530

    Minor styling fix 2.0

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
             <div className='flex justify-around lg:9/12 lg:pt-24 mx-auto bg-[url("https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWFydmVsfGVufDB8fDB8fA%3D%3D&w=1000&q=80")] bg-cover '>
               <div className='bg-[url("https://i.pinimg.com/originals/1f/2d/90/1f2d9088ff0c68825dc00096820313ff.png")] bg-contain bg-no-repeat text-center py-24 '>
                 <div className='font-bold lg:text-8xl font-koho flex flex-col justify-center text-center text-gray-900'> 
-                  <p className='text-gray-200 bg-gray-900 md:w-[98vw] w-screen py-2'>ARE YOU THE WORTHY ONE?</p>
+                  <p className='text-gray-200 bg-gray-900 md:w-[98.8vw] w-screen py-2'>ARE YOU THE WORTHY ONE?</p>
                 </div>
               </div>
             </div>
