6f3567676661fb6edf1308c1701c5c3935c94e09
SpywarePersues <spywarepersues284@gmail.com>
2022-10-15 09:20:40 +0530

    Styling chnages in sales page

--- pages/sale.js
+++ pages/sale.js
@@ -23,16 +23,16 @@ function infinityStones() {
     }
     return (
         <ContainerBlock title="Sale">
-            <div className='bg-[url("../public/moon.jpg")] py-10 bg-cover mb-10'>
-            <h1 className='py-5 mb-5 lg:text-5xl text-center text-gray-100 glassmorph'><span className='font-bold'>SALE</span> will be live this <span className='font-bold'>FULL MOON</span></h1>
-                <video autoPlay loop className='w-11/12 lg:w-5/12 mx-auto mb-5'>
+            <div className='py-10 bg-cover mb-10'>
+            <video autoPlay muted loop className='absolute w-screen min-screen'>
                     <source src='./moon.mp4' />
-                </video>
+            </video>
+            <h1 className='py-5 mb-5 lg:text-5xl text-center text-gray-100 glassmorph'><span className='font-bold'>SALE</span> will be live this <span className='font-bold'>FULL MOON</span></h1>
             </div>
                 <div className='flex flex-wrap justify-center mb-10'>
                     {firedata.map((data) => {
                         return(
-                            <a href={`/sale/${data.id}`} key={data.id} className="glassmorph w-4/12 mx-10">
+                            <a href={`/sale/${data.id}`} key={data.id} className="glassmorph md:mx-16 mx-6 w-[15rem] lg:w-[22rem] xl:w-[30rem] rounded-lg hover:scale-105 transition-scale duration-300 my-6">
                                 <img src={data.Image} className="w-screen lg:w-[46rem] border-solid border-slate-600 border-4 rounded-md" alt='' />
                             </a>
                         )
